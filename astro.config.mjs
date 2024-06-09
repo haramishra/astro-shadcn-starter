1e79ff0acbf303e2db3628b9dbc5c8304606855e
hara <haraprasad45@gmail.com>
2024-06-09 13:40:15 +0530

    utility packages installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,30 @@
-import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import { defineConfig } from "astro/config";
+import simpleStackForm from "simple-stack-form";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://astro-nomy.vercel.app",
   integrations: [
-    react(),
-    tailwind(),
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        theme: "github-dark-dimmed",
+      },
+      gfm: true,
+    }),
     icon(),
+    react(),
     tailwind({
       applyBaseStyles: false,
     }),
+    simpleStackForm(),
+    sitemap(),
   ],
+  output: "static",
 });
