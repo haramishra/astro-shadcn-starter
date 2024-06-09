7d9c2b1da2227a949a111e40e40bc1ad7fbc5b23
hara <haraprasad45@gmail.com>
2024-06-09 13:23:35 +0530

    landing ui added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
+import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -8,6 +8,7 @@ export default defineConfig({
   integrations: [
     react(),
     tailwind(),
+    icon(),
     tailwind({
       applyBaseStyles: false,
     }),
